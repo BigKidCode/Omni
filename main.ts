5d60f2f933cf740e29ed0f93c01dcca06580e178
BigKidCode <113431224+BigKidCode@users.noreply.github.com>
2022-09-13 16:58:10 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,454 @@
+/*
+load dependency
+"SuperBit": "file:../pxt-Superbit"
+*/
 
+//% color="#ECA40D" weight=20 icon="\u2708"
+
+namespace MAKEBRICK {
+
+    const PCA9685_ADD = 0x40
+    const MODE1 = 0x00
+    const MODE2 = 0x01
+    const SUBADR1 = 0x02
+    const SUBADR2 = 0x03
+    const SUBADR3 = 0x04
+
+    const LED0_ON_L = 0x06
+    const LED0_ON_H = 0x07
+    const LED0_OFF_L = 0x08
+    const LED0_OFF_H = 0x09
+
+    const ALL_LED_ON_L = 0xFA
+    const ALL_LED_ON_H = 0xFB
+    const ALL_LED_OFF_L = 0xFC
+    const ALL_LED_OFF_H = 0xFD
+
+    const PRESCALE = 0xFE
+
+    const STP_CHA_L = 2047
+    const STP_CHA_H = 4095
+
+    const STP_CHB_L = 1
+    const STP_CHB_H = 2047
+
+    const STP_CHC_L = 1023
+    const STP_CHC_H = 3071
+
+    const STP_CHD_L = 3071
+    const STP_CHD_H = 1023
+
+    let initialized = false
+    let yahStrip: neopixel.Strip;
+
+    export enum mwDigitalNum {
+        //% blockId="P16P15" block="P16P15"
+        P16P15 = 1,
+    }
+
+    export enum enMusic {
+
+        dadadum = 0,
+        entertainer,
+        prelude,
+        ode,
+        nyan,
+        ringtone,
+        funk,
+        blues,
+
+        birthday,
+        wedding,
+        funereal,
+        punchline,
+        baddy,
+        chase,
+        ba_ding,
+        wawawawaa,
+        jump_up,
+        jump_down,
+        power_up,
+        power_down
+    }
+
+
+
+    export enum enSteppers {
+        B1 = 0x1,
+        B2 = 0x2
+    }
+    export enum enPos {
+        //% blockId="forward" block="forward"
+        forward = 1,
+        //% blockId="reverse" block="reverse"
+        reverse = 2,
+        //% blockId="stop" block="stop"
+        stop = 3
+    }
+
+    export enum enDuration {
+
+        //% block="1"
+        d1 = 1000,
+        //% block="2"
+        d2 = 2000,
+        //% block="3"
+        d3 = 3000,
+        //% block="4"
+        d4 = 4000,
+        //% block="5"
+        d5 = 5000,
+        //% block="6"
+        d6 = 6000,
+    }
+
+    export enum enDirection {
+
+        //% block="Forward"
+        di1 = 1,
+        //% block="Backward"
+        di2 = -1,
+    }
+
+    export enum enDuration2 {
+
+        //% block="0.4"
+        d1 = 400,
+        //% block="0.5"
+        d2 = 500,
+        //% block="0.6"
+        d3 = 600,
+        //% block="0.7"
+        d4 = 700
+    }
+
+    export enum enDirection2 {
+
+        //% block="Left"
+        di1 = 1,
+        //% block="Right"
+        di2 = -1,
+    }
+
+    export enum enMotors {
+        M1 = 8,
+        M2 = 10,
+        M3 = 12,
+        M4 = 14
+    }
+
+    export enum enTurns {
+        //% blockId="T1B4" block="1/4"
+        T1B4 = 90,
+        //% blockId="T1B2" block="1/2"
+        T1B2 = 180,
+        //% blockId="T1B0" block="1"
+        T1B0 = 360,
+        //% blockId="T2B0" block="2"
+        T2B0 = 720,
+        //% blockId="T3B0" block="3"
+        T3B0 = 1080,
+        //% blockId="T4B0" block="4"
+        T4B0 = 1440,
+        //% blockId="T5B0" block="5"
+        T5B0 = 1800
+    }
+
+    export enum enServo {
+
+        S1 = 0,
+        S2,
+        S3,
+        S4,
+        S5,
+        S6,
+        S7,
+        S8
+    }
+
+    function i2cwrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2ccmd(addr: number, value: number) {
+        let buf = pins.createBuffer(1)
+        buf[0] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2cread(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+
+    function initPCA9685(): void {
+        i2cwrite(PCA9685_ADD, MODE1, 0x00)
+        setFreq(50);
+        initialized = true
+    }
+
+    function setFreq(freq: number): void {
+        // Constrain the frequency
+        let prescaleval = 25000000;
+        prescaleval /= 4096;
+        prescaleval /= freq;
+        prescaleval -= 1;
+        let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
+        let oldmode = i2cread(PCA9685_ADD, MODE1);
+        let newmode = (oldmode & 0x7F) | 0x10; // sleep
+        i2cwrite(PCA9685_ADD, MODE1, newmode); // go to sleep
+        i2cwrite(PCA9685_ADD, PRESCALE, prescale); // set the prescaler
+        i2cwrite(PCA9685_ADD, MODE1, oldmode);
+        control.waitMicros(5000);
+        i2cwrite(PCA9685_ADD, MODE1, oldmode | 0xa1);
+    }
+
+    function setPwm(channel: number, on: number, off: number): void {
+        if (channel < 0 || channel > 15)
+            return;
+        if (!initialized) {
+            initPCA9685();
+        }
+        let buf = pins.createBuffer(5);
+        buf[0] = LED0_ON_L + 4 * channel;
+        buf[1] = on & 0xff;
+        buf[2] = (on >> 8) & 0xff;
+        buf[3] = off & 0xff;
+        buf[4] = (off >> 8) & 0xff;
+        pins.i2cWriteBuffer(PCA9685_ADD, buf);
+    }
+
+    function setStepper(index: number, dir: boolean): void {
+        if (index == enSteppers.B1) {
+            if (dir) {
+                setPwm(11, STP_CHA_L, STP_CHA_H);
+                setPwm(9, STP_CHB_L, STP_CHB_H);
+                setPwm(10, STP_CHC_L, STP_CHC_H);
+                setPwm(8, STP_CHD_L, STP_CHD_H);
+            } else {
+                setPwm(8, STP_CHA_L, STP_CHA_H);
+                setPwm(10, STP_CHB_L, STP_CHB_H);
+                setPwm(9, STP_CHC_L, STP_CHC_H);
+                setPwm(11, STP_CHD_L, STP_CHD_H);
+            }
+        } else {
+            if (dir) {
+                setPwm(12, STP_CHA_L, STP_CHA_H);
+                setPwm(14, STP_CHB_L, STP_CHB_H);
+                setPwm(13, STP_CHC_L, STP_CHC_H);
+                setPwm(15, STP_CHD_L, STP_CHD_H);
+            } else {
+                setPwm(15, STP_CHA_L, STP_CHA_H);
+                setPwm(13, STP_CHB_L, STP_CHB_H);
+                setPwm(14, STP_CHC_L, STP_CHC_H);
+                setPwm(12, STP_CHD_L, STP_CHD_H);
+            }
+        }
+    }
+
+    function stopMotor(index: number) {
+        setPwm(index, 0, 0);
+        setPwm(index + 1, 0, 0);
+    }
+    /**
+     * *****************************************************************
+     * @param index
+     */
+    //% blockId=SuperBit_RGB_Program block="RGB_Program"
+    //% weight=100
+    //% blockGap=10
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function RGB_Program(): neopixel.Strip {
+
+        if (!yahStrip) {
+            yahStrip = neopixel.create(DigitalPin.P12, 4, NeoPixelMode.RGB);
+        }
+        return yahStrip;
+    }
+
+
+    //% blockId=SuperBit_Servo3 block="Servo(360°)|num %num|pos %pos|value %value"
+    //% weight=92
+    //% blockGap=10
+    //% num.min=1 num.max=4 value.min=0 value.max=90
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=20
+    export function Servo3(num: enServo, pos: enPos, value: number): void {
+
+        // 50hz: 20,000 us
+
+        if (pos == enPos.stop) {
+            let us = (86 * 1800 / 180 + 600); // 0.6 ~ 2.4
+            let pwm = us * 4096 / 20000;
+            setPwm(num, 0, pwm);
+        }
+        else if (pos == enPos.forward) { //0-90 -> 90 - 0
+            let us = ((90 - value) * 1800 / 180 + 600); // 0.6 ~ 2.4
+            let pwm = us * 4096 / 20000;
+            setPwm(num, 0, pwm);
+        }
+        else if (pos == enPos.reverse) { //0-90 -> 90 -180
+            let us = ((90 + value) * 1800 / 180 + 600); // 0.6 ~ 2.4
+            let pwm = us * 4096 / 20000;
+            setPwm(num, 0, pwm);
+        }
+
+
+
+    }
+    //% blockId=SuperBit_MotorRun block="Motor|%index|speed(-255~255) %speed"
+    //% weight=91
+    //% speed.min=-255 speed.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function MotorRun(index: enMotors, speed: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        speed = speed * 16; // map 255 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= -4096) {
+            speed = -4095
+        }
+
+        let a = index
+        let b = index + 1
+
+        if (a > 10) {
+            if (speed >= 0) {
+                setPwm(a, 0, speed)
+                setPwm(b, 0, 0)
+            } else {
+                setPwm(a, 0, 0)
+                setPwm(b, 0, -speed)
+            }
+        }
+        else {
+            if (speed >= 0) {
+                setPwm(b, 0, speed)
+                setPwm(a, 0, 0)
+            } else {
+                setPwm(b, 0, 0)
+                setPwm(a, 0, -speed)
+            }
+        }
+
+    }
+
+    //% block="Car Run | %direction"
+    //% weight=99
+    export function CarRun(direction: enDirection): void {
+        MotorRun(enMotors.M1, 255 * direction)
+        MotorRun(enMotors.M2, 255 * direction)
+        MotorRun(enMotors.M3, 255 * direction)
+        MotorRun(enMotors.M4, 255 * direction)
+    }
+
+    //% block="Car Run | %direction|for %duration ms"
+    //% weight=98
+    export function CarRunD(direction: enDirection, duration: number): void {
+        MotorRun(enMotors.M1, 255 * direction)
+        MotorRun(enMotors.M2, 255 * direction)
+        MotorRun(enMotors.M3, 255 * direction)
+        MotorRun(enMotors.M4, 255 * direction)
+        basic.pause(duration)
+        MotorStopAll()
+    }
+
+    //% block="Car Turn | %direction"
+    //% weight=97
+    export function CarTurn(direction: enDirection2): void {
+            MotorRun(enMotors.M1, 255 * direction * -1)
+            MotorRun(enMotors.M2, 255 * direction * -1)
+            MotorRun(enMotors.M3, 255 * direction)
+            MotorRun(enMotors.M4, 255 * direction)
+    }
+
+    //% block="Car Turn | %direction|for %duration ms"
+    //% weight=96
+    export function CarTurnD(direction: enDirection2, duration: number): void {
+        MotorRun(enMotors.M1, 255 * direction * -1)
+        MotorRun(enMotors.M2, 255 * direction * -1)
+        MotorRun(enMotors.M3, 255 * direction)
+        MotorRun(enMotors.M4, 255 * direction)
+        basic.pause(duration)
+        MotorStopAll()
+    }
+
+    //% block="Car Drift | %direction"
+    //% weight=95
+    export function CarDrift(direction: enDirection2): void {
+        MotorRun(enMotors.M1, 255 * direction * -1)
+        MotorRun(enMotors.M2, 255 * direction)
+        MotorRun(enMotors.M3, 255 * direction)
+        MotorRun(enMotors.M4, 255 * direction * -1)
+    }
+
+    //% block="Car Drift | %direction|for %duration ms"
+    //% weight=94
+    export function CarDriftD(direction: enDirection2, duration: number): void {
+        MotorRun(enMotors.M1, 255 * direction * -1)
+        MotorRun(enMotors.M2, 255 * direction)
+        MotorRun(enMotors.M3, 255 * direction)
+        MotorRun(enMotors.M4, 255 * direction * -1)
+        basic.pause(duration)
+        MotorStopAll()
+    }
+
+    //% block="Run Until Obstacle |in %distance cm"
+    //% weight=93
+    export function RunUntilObstacle(distance: number): void {
+        while (true){
+            CarRun(1)
+            if (Ultrasonic(1) < distance) {
+                basic.pause(100)
+                if (Ultrasonic(1) < distance) {
+                    break;
+                }
+            }
+        }
+        MotorStopAll()
+    }
+
+    //% blockId=SuperBit_MotorStopAll block="Motor Stop All"
+    //% weight=91
+    //% blockGap=50
+    export function MotorStopAll(): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+
+        stopMotor(enMotors.M1);
+        stopMotor(enMotors.M2);
+        stopMotor(enMotors.M3);
+        stopMotor(enMotors.M4);
+
+    }
+
+    //% blockId=ModuleWorld_Digital_Ultrasonic block="Ultrasonic|pin %value_DNum"
+    //% weight=92
+    //% blockGap=20
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5
+    export function Ultrasonic(value_DNum: mwDigitalNum): number {
+        //send pulse
+        let Trig, Echo;
+        if (value_DNum == 1) { Trig = DigitalPin.P16; Echo = DigitalPin.P15; }
+
+        pins.setPull(Trig, PinPullMode.PullNone);
+        pins.digitalWritePin(Trig, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(Trig, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(Trig, 0);
+
+        //read pulse, maximum distance=500cm
+        const d = pins.pulseIn(Echo, PulseValue.High, 500 * 58);
+
+        return Math.idiv(d, 58);
+    }
+
+}
